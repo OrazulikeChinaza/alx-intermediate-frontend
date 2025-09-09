e588428cf3e4c5ec03c0ecb1eaae43939efe7326
Orazulike Chinaza <orazulikechinaza@gmail.com>
2025-09-10 00:31:52 +0100

    1-index-html file addition

---
+++ 0x02-tailwind-css/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./**/*.html", "./**/*.js"],  // Scans all HTML/JS in 0x02-tailwind-css/
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
